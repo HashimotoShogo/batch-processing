a18f8ad65fa0286fbc5d6ca12346b327352effc2
HashimotoShogo <shogo.hashimoto@money-design.com>
2019-10-01 17:34:46 +0900

    Create an Intermediate Processor　add coment

--- src/main/java/com/example/batchprocessing/PersonItemProcessor.java
+++ src/main/java/com/example/batchprocessing/PersonItemProcessor.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
+//中間処理を行うクラス
 public class PersonItemProcessor implements ItemProcessor<Person,Person> {
 
     private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
 
     @Override
     public Person process(final Person person) throws Exception{
-        final String firstName = person.getFirstName().toUpperCase();
-        final String lastName = person.getLastName().toUpperCase();
+        final String firstName = person.getFirstName().toUpperCase();//firstNameを大文字に変換
+        final String lastName = person.getLastName().toUpperCase();  //lastNameを大文字に変換
 
         final Person transformedPerson = new Person(firstName, lastName);
 
