4125b7cfc08ac0f2fb5c3408f243c1e468255ce6
HashimotoShogo <shogo.hashimoto@money-design.com>
2019-10-11 12:15:20 +0900

    modify docker-compose,init.sql,Dockerfile

--- local-mysql/db/init.sql
+++ local-mysql/db/init.sql
@@ -8,3 +8,11 @@ CREATE TABLE people  (
 );
 
 insert into people (first_name,last_name) values ('Jill','Doe'),('Joe','Doe'),('Justin','Doe'),('Jane','Doe'),('John','Doe');
+
+CREATE TABLE address1 (
+    address_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    address1 VARCHAR(20),
+    people_id BIGINT
+);
+
+insert into address1 (address1,people_id) values ('Jill_house',1),('Joe_house',2),('justin_home',3),('jane_house',4),('John_house',5);
