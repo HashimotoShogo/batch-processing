ae441fbff62c9cffcd836541e7f3c9d88418b541
HashimotoShogo <shogo.hashimoto@money-design.com>
2019-10-16 17:18:29 +0900

    create RepositoryTestClass

--- src/main/java/com/example/batchprocessing/repository/PersonRepository.java
+++ src/main/java/com/example/batchprocessing/repository/PersonRepository.java
@@ -1,14 +1,10 @@
 package com.example.batchprocessing.Repository;
 
 
-import com.example.batchprocessing.SettingMysql;
 import com.example.batchprocessing.person.Person;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
+import jooq.human.tables.Address1;
+import org.jooq.*;
 import org.jooq.impl.DSL;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static jooq.human.tables.People.PEOPLE;
+import static jooq.human.tables.Address1.ADDRESS1;
 
 @Repository
 @Transactional(readOnly = true)
@@ -36,16 +33,16 @@ public class PersonRepository {
     //@Value("${spring.datasource.password}")
     String password="root";
 
-    public List<Person> getAllPersonInfo() throws SQLException {
+    public List<Person> getAllPersonInfo() throws SQLException { //SQLメモ書き　select first_name,last_name,address1 from people inner join address1 on people.person_id = address1.people_id;
         List<Person> list = new ArrayList<>();
 
         try (Connection con = DriverManager.getConnection(url, username, password)) {
 
             DSLContext dslContext = DSL.using(con, SQLDialect.MYSQL_5_7);
 
-            Result<Record> result = dslContext.select().from(PEOPLE).fetch();
-            
-            result.forEach(record -> list.add(new Person(record.get(PEOPLE.PERSON_ID), record.getValue(PEOPLE.FIRST_NAME), record.getValue(PEOPLE.LAST_NAME))));
+            Result<Record4<Long,String,String,String>> result = dslContext.select(PEOPLE.PERSON_ID ,PEOPLE.FIRST_NAME ,PEOPLE.LAST_NAME ,ADDRESS1.ADDRESS1_).from(PEOPLE).join(ADDRESS1).on(PEOPLE.PERSON_ID.eq(ADDRESS1.PEOPLE_ID)).fetch();
+
+            result.forEach(record -> list.add(new Person(record.get(PEOPLE.PERSON_ID), record.getValue(PEOPLE.FIRST_NAME), record.getValue(PEOPLE.LAST_NAME),record.getValue(ADDRESS1.ADDRESS1_))));
 
             return list;
         }
--- src/main/java/com/example/batchprocessing/service/PersonService.java
+++ src/main/java/com/example/batchprocessing/service/PersonService.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,13 +28,19 @@ public class PersonService implements CommandLineRunner {
         log.info("DBから情報を取り出します。");
         log.info("Start extract person info");
 
-
         List<Person> list = takePersonInfo();
 
 
         log.info("Finished extracting person info ");
         log.info("personの情報を表示します。");
         list.forEach(Person -> System.out.println(Person));
+
+        log.info("取得した情報の出力を始めます。");
+
+        writeCSVtoLocal(list);
+
+        log.info("CSV出力が終了しました。");
+
     }
 
 
@@ -43,5 +53,26 @@ public class PersonService implements CommandLineRunner {
             e.printStackTrace();
         }
         return list;
+    }
+
+
+    public void writeCSVtoLocal(List<Person> personList){
+
+        for(Person person:personList){
+            StringBuilder str = new StringBuilder();
+
+            str.append(person.getFirstName()+","+person.getLastName()+","+person.getAddress());
+
+            File file = new File("USER/デスクトップ/test.csv");
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+
+                bw.write(str.toString());
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
