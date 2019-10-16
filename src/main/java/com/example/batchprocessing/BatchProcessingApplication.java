2f6d72f8bf03c21e261c6eb066f0c90adf419d6a
HashimotoShogo <shogo.hashimoto@money-design.com>
2019-10-16 15:52:27 +0900

    modify for using service class

--- src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,13 +1,16 @@
 package com.example.batchprocessing;
 
+import com.example.batchprocessing.Service.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application.properties.xml")
 public class BatchProcessingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchProcessingApplication.class, args);
+		SpringApplication.run(PersonService.class, args);
 	}
 
 }
