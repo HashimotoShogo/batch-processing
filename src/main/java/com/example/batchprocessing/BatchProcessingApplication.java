96cb1ced8ed62331e5cf41056a97ea2bf98634b2
HashimotoShogo <53550856+HashimotoShogo@users.noreply.github.com>
2019-10-28 17:25:58 +0900

    Revert "Jooqを利用して情報を取得する。"

--- src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,25 +1,13 @@
 package com.example.batchprocessing;
 
-import com.example.batchprocessing.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ImportResource("classpath:application.properties.xml")
-public class BatchProcessingApplication implements CommandLineRunner {
+public class BatchProcessingApplication {
 
-    @Autowired
-    PersonService personService;
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessingApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(BatchProcessingApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
-        personService.run("hogefuga");
-    }
 }
